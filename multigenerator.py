633ab0b308b69baaad18a3ad341459b1b8886d60
Pablo Ventura <pablo@frege>
2017-02-01 19:08:46 -0300

    multi model generator

---
+++ multigenerator.py
@@ -0,0 +1,14 @@
+import os
+
+universe = 30
+path = "positives/"
+for arity in range(2,5):
+    for density in [0.1,0.2,0.3,0.4,0.5]:
+        for i in range(20):
+            s = "python generator.py"
+            s += " -u%s -a%s -d%s > %s" % (universe,arity,density,path)
+            s += "p%s_%s_%s_%s.model" % (i,universe,arity,density)
+            print("Generating %sp%s_%s_%s_%s.model" % (path,i,universe,arity,density))
+            os.system(s)
+
+print("GENERATION FINISHED")
