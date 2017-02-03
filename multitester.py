2267b86b2761f463de728f7307070203dbd4525d
Pablo Ventura <pablo@frege>
2017-02-03 03:35:56 -0300

    test run only file results not exists

--- multitester.py
+++ multitester.py
@@ -8,6 +8,9 @@ import glob, os
 for f in glob.glob(path + "*.model"):
     print ("Processing %s" % f)
     out = f[:-6]+".result"
-    os.system("{ /usr/bin/time -v timeout --signal=SIGINT %s python main.py < %s;} >%s 2>>%s" % (timeout,f,out,out))
+    if not os.path.isfile(out) :
+        os.system("{ /usr/bin/time -v timeout --signal=SIGINT %s python main.py < %s;} >%s 2>>%s" % (timeout,f,out,out))
+    else:
+        print ("File %s already exists" % out)
 
 print("PROCESSING FINISHED")
