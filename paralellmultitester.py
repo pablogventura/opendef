35cbdda6d18484e16de475de0e5752b6d54c400b
Pablo Ventura <pablogventura@gmail.com>
2017-02-22 11:34:02 -0300

    pararell multi tester

--- paralellmultitester.py
+++ paralellmultitester.py
@@ -1,25 +1,54 @@
 import os
-
-path = "positives12/"
+import multiprocessing
+import glob, os
+import subprocess as sp
+path = "positives/"
 timeout = "30m"
-
 
 running = []
 waiting = []
 
-for p in running:
-    if p.poll() is not None:
-        running.remove(p)
-        running.append(In [119]: p=sp.Popen(["pi","9999"],stdout=sp.PIPE)
+cores = multiprocessing.cpu_count()
+print ("Detected %s cores..." % cores)
+
+for filein in glob.glob(path + "*2_0.1.model"):
+
+    fileout = filein[:-6]+".result"
+    if not os.path.isfile(fileout) :
+        waiting.append((filein,["/usr/bin/time", "-v", "timeout", "--signal=SIGINT", timeout, "python", "main.py"],fileout))
+    else:
+        print ("File %s already exists" % fileout)
+
+
+for i in range(cores):
+    try:
+        filein,call,fileout = waiting.pop()
+    except IndexError:
+        break
+    print ("Processing %s" % filein)
+    filein  = open(filein,"r")
+    fileout = open(fileout, "w")
+    running.append((sp.Popen(call,stdin=filein,stdout=fileout,stderr=fileout),filein,fileout))
+    
+
+while running:
+    for p,fi,fo in running:
+        if p.poll() is not None:
+            #delete finished
+            fi.close()
+            fo.close()
+            running.remove((p,fi,fo))
+            #run new
+            try:
+                filein,call,fileout = waiting.pop()
+                print ("Processing %s" % filein)
+                filein  = open(filein,"r")
+                fileout = open(fileout, "w")
+                running.append((sp.Popen(call,stdin=filein,stdout=fileout,stderr=fileout),filein,fileout))
+            except IndexError:
+                continue
         
 
-import glob, os
-for f in glob.glob(path + "*.model"):
-    print ("Processing %s" % f)
-    out = f[:-6]+".result"
-    if not os.path.isfile(out) :
-        os.system("{ /usr/bin/time -v timeout --signal=SIGINT %s python main.py < %s;} >%s 2>>%s" % (timeout,f,out,out))
-    else:
-        print ("File %s already exists" % out)
+
 
 print("PROCESSING FINISHED")
