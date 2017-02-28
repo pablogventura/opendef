ed7406b2f9dda7f7953d0965ddef601f1a6d33e7
Pablo Ventura <pablogventura@gmail.com>
2017-02-28 15:06:46 -0300

    improving graphics

--- processresults.py
+++ processresults.py
@@ -1,20 +1,20 @@
 from collections import namedtuple, defaultdict
 import os
+import glob, os
 from types import SimpleNamespace
 
 
 path = "positives/"
 
-data = defaultdict(lambda : defaultdict(lambda : SimpleNamespace(max_s=float("-inf"),
-                                                                 min_s=float("inf"),
-                                                                 average_s=0,
-                                                                 max_time=float("-inf"),
-                                                                 min_time=float("inf"),
-                                                                 average_time=0,
-                                                                 succesfull=0,
-                                                                 timeouts=0)))
+data = defaultdict(lambda : defaultdict(lambda : defaultdict(lambda : SimpleNamespace(max_s=float("-inf"),
+                                                                                      min_s=float("inf"),
+                                                                                      average_s=0,
+                                                                                      max_time=float("-inf"),
+                                                                                      min_time=float("inf"),
+                                                                                      average_time=0,
+                                                                                      succesfull=0,
+                                                                                      timeouts=0)))
 
-import glob, os
 for f in glob.glob(path + "*.result"):
     print ("Processing %s" % f)
     #p10_30_2_0.2.result
@@ -31,9 +31,9 @@ for f in glob.glob(path + "*.result"):
         s = file.readline()
     
     file.close()
-    i,universe,arity,density = f[len(path)+1:-len(".result")].split("_")
+    i,size,arity,density = f[len(path)+1:-len(".result")].split("_")
     i=int(i)
-    universe=int(universe)
+    size=int(size)
     arity=int(arity)
     density = float(density)
     
@@ -53,38 +53,37 @@ for f in glob.glob(path + "*.result"):
         #data[arity][density].average_time=None
         
         #data[arity][density].succesfull=0
-        data[arity][density].timeouts+=1
+        data[size][arity][density].timeouts+=1
     else:
         #succesful case 
-        if data[arity][density].max_s <= diversity:
-            data[arity][density].max_s=diversity
-        if data[arity][density].min_s >= diversity:
-            data[arity][density].min_s=diversity
-        data[arity][density].average_s+=diversity
+        if data[size][arity][density].max_s <= diversity:
+            data[size][arity][density].max_s=diversity
+        if data[size][arity][density].min_s >= diversity:
+            data[size][arity][density].min_s=diversity
+        data[size][arity][density].average_s+=diversity
         
-        if data[arity][density].max_time <= time:
-            data[arity][density].max_time=time
-        if data[arity][density].min_time >= time:
-            data[arity][density].min_time=time
-        data[arity][density].average_time+=time
+        if data[size][arity][density].max_time <= time:
+            data[size][arity][density].max_time=time
+        if data[size][arity][density].min_time >= time:
+            data[size][arity][density].min_time=time
+        data[size][arity][density].average_time+=time
         
-        data[arity][density].succesfull+=1
+        data[size][arity][density].succesfull+=1
         
     
-    
-for arity in range(2,5):
-    for density in [0.1,0.2,0.3,0.4,0.5]:
-        try:
-            data[arity][density].average_s/=data[arity][density].succesfull
-            data[arity][density].average_time/=data[arity][density].succesfull
-        except ZeroDivisionError:
-            data[arity][density].average_s=None
-            data[arity][density].average_time=None
+for size in [10000,20000,30000,40000]:
+    for arity in range(2,5):
+        for density in [0.1,0.3,0.5]:
+            try:
+                data[size][arity][density].average_s/=data[size][arity][density].succesfull
+                data[size][arity][density].average_time/=data[size][arity][density].succesfull
+            except ZeroDivisionError:
+                data[size][arity][density].average_s=None
+                data[size][arity][density].average_time=None
 
 
 print("PROCESSING FINISHED")
 print("")
-print (data)
 
 
 import numpy as np
@@ -110,11 +109,10 @@ x=[]
 y=[]
 for density in [0.1,0.3,0.5]:
     x.append(density)
-    if data[5][density].average_time:
-        y.append(data[5][density].average_time)
+    if data[4][density].average_time:
+        y.append(data[4][density].average_time)
     else:
         y.append(0)
-    print(data[5][density].average_time)
 #plt.plot(x, y, color="blue", linewidth=1.0, linestyle="-")
 
 #plt.yscale('log')
