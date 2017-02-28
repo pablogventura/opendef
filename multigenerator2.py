0ceb753c3602b9f983d3fe52edc29c2545342813
Pablo Ventura <pablogventura@gmail.com>
2017-02-28 18:56:41 -0300

    multigenerator

--- multigenerator2.py
+++ multigenerator2.py
@@ -3,7 +3,7 @@ import os
 path = "positives/"
 for arity in [2,3,4]:
     for density in [0.1,0.3,0.5]:
-        for size in [10000,20000,30000,40000]:
+        for size in [15000,25000,35000]:
             for i in range(50):
                 s = "python3 generator2.py"
                 s += " -s%s -a%s -d%s > %s" % (size,arity,density,path)
