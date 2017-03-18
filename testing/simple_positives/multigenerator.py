c22c4ff19265a664ca3f9d0bb619de8a3a874473
Pablo Ventura <pablogventura@gmail.com>
2017-03-18 17:58:29 -0300

    fix

--- testing/simple_positives/multigenerator.py
+++ testing/simple_positives/multigenerator.py
@@ -4,7 +4,7 @@ path="data/"
 
 arity = 2
 for quantity in range(1,4+1,1):
-    for density in [0.1]:#,0.2,0.3,0.4,0.5]:
+    for density in [0.1,0.2,0.3,0.4,0.5]:
         for universe in range(50,100+1,10):
             for i in range(50):
                 s = "python3 positive_model_generator.py"
@@ -14,7 +14,7 @@ for quantity in range(1,4+1,1):
                 os.system(s)
 arity = 3
 for quantity in range(1,1+1,1):
-    for density in [0.1]:#,0.2,0.3,0.4,0.5]:
+    for density in [0.1,0.2,0.3,0.4,0.5]:
         for universe in range(23,27+1,1):
             for i in range(50):
                 s = "python3 positive_model_generator.py"
