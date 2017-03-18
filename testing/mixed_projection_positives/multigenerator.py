230a4b6de1b94cad9242ec30142581b559a2ec4a
Pablo Ventura <pablogventura@gmail.com>
2017-03-18 18:29:52 -0300

    fix

--- testing/mixed_projection_positives/multigenerator.py
+++ testing/mixed_projection_positives/multigenerator.py
@@ -3,7 +3,7 @@ import os
 path="data/"
 
 
-for density in [0.1]:#,0.2,0.3,0.4,0.5]:
+for density in [0.1,0.2,0.3,0.4,0.5]:
     for universe in range(50,100+1,10):
         for i in range(50):
             s = "python3 projection_model_generator.py"
--- testing/random/multigenerator.py
+++ testing/random/multigenerator.py
@@ -4,12 +4,13 @@ path="data/"
 
 arity=2
 quantity=4
-for density in [0.1]:#,0.2,0.3,0.4,0.5]:
+for density in [0.1,0.2,0.3,0.4,0.5]:
     for universe in range(50,100+1,10):
         for i in range(50):
             s = "python3 random_model_generator.py"
             s += " -d%s -a%s -u%s -q%s > %s" % (density,arity,universe,quantity,path)
             s += "p%s_%s_%s_%s_q%s.model" % (i,density,arity,universe,quantity)
             print("Generating %sp%s_%s_%s_%s_q%s.model" % (path,i,density,arity,universe,quantity))
+            os.system(s)
      
 print("GENERATION FINISHED")
