98a89c04b2f1a2f582d23c6361771c18036ad3a2
Pablo Ventura <pablo@claude.workgroup>
2018-02-02 10:07:09 -0300

    refactoring

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python   
+
+import sys
+from itertools import chain,count
+
+from parser import stdin_parser
 from counterexample import Counterexample
 from minion import is_isomorphic
-from parser import stdin_parser
 from minion import automorphisms, isomorphisms, is_isomorphic_to_any, MinionSol
-from itertools import chain,count
-from misc import indent
-from collections import defaultdict
-import operator as op
-from functools import reduce
-import resource
-import sys
+from misc import indent, ncr, childrens_time
+from setsized import SetSized
+from optparse import OptionParser
 
-verbose=sys.stdout.isatty()
-
+verbose=sys.stdout.isatty() # si es a la consola, es verboso
+gen_tree=False
 latex_tree =""
 
-def childrens_time():
-    time = resource.getrusage(resource.RUSAGE_CHILDREN)
-    time = time[0]+time[1] #user + system
-    return time
-    
-def ncr(n, r):
-    r = min(r, n-r)
-    if r == 0: return 1
-    numer = reduce(op.mul, range(n, n-r, -1))
-    denom = reduce(op.mul, range(1, r+1))
-    return numer//denom
     
 def main():
+    global verbose
+    global gen_tree
+    
+    parser = OptionParser()
+    parser.add_option("-v", action="store_true", dest="verbose", help="Verbose mode, default when stdout is a tty")
+    parser.add_option("-t", action="store_true", dest="gen_tree", help="Generates 'tree.tex' file with traversed tree")
+    (options, args) = parser.parse_args()
+    
+    verbose = verbose or options.verbose
+    gen_tree=options.gen_tree
+        
     model = stdin_parser()
     targets_rel = tuple(sym for sym in model.relations.keys() if sym[0]=="T")
     if not targets_rel:
@@ -36,29 +36,7 @@ def main():
         return
     is_open_rel(model,targets_rel)
 
-class SetSized(object):
-    def __init__(self,values=[]):
-        self.dict = defaultdict(set)
-        for v in values:
-            self.add(v)
-    
-    def add(self,e):
-        self.dict[len(e)].add(e)
-    
-    def __iter__(self):
-        print("WARNING: __iter__ SetSized")
-        for i in self.sizes():
-            for v in self.iterate(i):
-                yield v
 
-    def __len__(self):
-        return sum(self.len(s) for s in self.sizes())
-    def len(self,size):
-        return len(self.dict[size])
-    def sizes(self):
-        return sorted(self.dict.keys())
-    def iterate(self,size):
-        return iter(self.dict[size])
 
 class GenStack(object):
     def __init__(self, generator,total=None, pp_d=[]):
@@ -72,6 +50,7 @@ class GenStack(object):
     def next(self):
         global latex_tree
         global verbose
+        global gen_tree
         result = None
         while result is None or frozenset(result.universe) in self.history:
             try:
@@ -89,20 +68,24 @@ class GenStack(object):
             self.tabs +=1
         elif self.old_total < total:
             self.tabs -=1
-            latex_tree+=("  " * self.tabs)+"]\n"
-            latex_tree+=("  " * self.tabs)+"]\n"
+            if gen_tree:
+                latex_tree+=("  " * self.tabs)+"]\n"
+                latex_tree+=("  " * self.tabs)+"]\n"
         else:
-            latex_tree+=("  " * self.tabs)+"]\n"
+            if gen_tree:
+                latex_tree+=("  " * self.tabs)+"]\n"
             
         self.old_total = total
         if verbose:
-            latex_tree+="  " * self.tabs # dejo los tabs para que se agregue el subset
+            if gen_tree:
+                latex_tree+="  " * self.tabs # dejo los tabs para que se agregue el subset
             print (("Subset %s of %s    \tDiversity:%s" % (i,total,len(self.pp_d)))+30*" ", end="\r")
         return result
 
 
 def is_open_rel(model, target_rels):
     global latex_tree
+    global gen_tree
     base_rels = tuple((r for r in model.relations if r not in target_rels))
     spectrum = sorted(model.spectrum(target_rels),reverse=True)
     if spectrum:
@@ -123,12 +106,14 @@ def is_open_rel(model, target_rels):
                 break
             iso = is_isomorphic_to_any(current, S, base_rels)
             if iso:
-                latex_tree+="[\\{{%s}\\}\n" % ",".join(str(i) for i in sorted(current.universe))
+                if gen_tree:
+                    latex_tree+="[\\{{%s}\\}\n" % ",".join(str(i) for i in sorted(current.universe))
                 isos_count += 1
                 if not iso.iso_wrt(target_rels):
                     raise Counterexample(iso)
             else:
-                latex_tree+="[\\{{%s}\\},auts\n" % ",".join(str(i) for i in sorted(current.universe))
+                if gen_tree:
+                    latex_tree+="[\\{{%s}\\},auts\n" % ",".join(str(i) for i in sorted(current.universe))
                 for aut in automorphisms(current,base_rels):
                     auts_count += 1
                     if not aut.aut_wrt(target_rels):
@@ -162,18 +147,20 @@ def is_open_rel(model, target_rels):
     print("  %s calls to Minion" % MinionSol.count)
     print("  Minion total time = %s secs" % childrens_time())
     print("")
-    latex_tree = ("[\\{{%s}\\}\n" % ",".join(str(i) for i in sorted(model.universe))) + latex_tree
-    latex_tree +="]]\n"
     
-    ftarget = open("tree.tex","w")
-    base = open("base1.tex","r")
-    ftarget.write(base.read())
-    base.close()
-    ftarget.write(latex_tree[:-1])
-    base = open("base2.tex","r")
-    ftarget.write(base.read())
-    base.close()
-    ftarget.close()
+    if gen_tree:
+        latex_tree = ("[\\{{%s}\\}\n" % ",".join(str(i) for i in sorted(model.universe))) + latex_tree
+        latex_tree +="]]\n"
+        
+        ftarget = open("tree.tex","w")
+        base = open("latex/base1.tex","r")
+        ftarget.write(base.read())
+        base.close()
+        ftarget.write(latex_tree[:-1])
+        base = open("latex/base2.tex","r")
+        ftarget.write(base.read())
+        base.close()
+        ftarget.close()
 
 if __name__ == "__main__":
     main()
